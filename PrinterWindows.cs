53259cccfbe086a9948c1231cd8f6f282c024280
Ahmadr08 <ahmad.2304@students.amikom.ac.id>
2020-05-07 12:49:09 +0700

    Polymorphism Sederhana

---
+++ PrinterWindows.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphism
+{
+    class PrinterWindows
+    {
+        public virtual void Show()
+        {
+            Console.WriteLine("");
+        }
+        public virtual void Print()
+        {
+            Console.WriteLine("");
+        }
+    }
+
+    class Canon : PrinterWindows
+    {
+        public override void Show()
+        {
+            base.Show();
+            Console.Write("\tCanon Display dimension is 9.5 x 12");
+        }
+        public override void Print()
+        {
+            base.Print();
+            Console.Write("\tCanon printer printing...");
+        }
+    }
+
+    class Epson : PrinterWindows
+    {
+        public override void Show()
+        {
+            base.Show();
+            Console.Write("\tEpson Display Dimension is 10 x 11");
+        }
+        public override void Print()
+        {
+            base.Print();
+            Console.Write("\tEpson printer printing...");
+        }
+    }
+
+    class LaserJet : PrinterWindows
+    {
+        public override void Show()
+        {
+            base.Show();
+            Console.Write("\tLaser Jet Display Dimension is 12 x 12");
+        }
+        public override void Print()
+        {
+            base.Print();
+            Console.Write("\tLaser Jet printer printing...");
+        }
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrinterWindows Printer;
+
+            Console.WriteLine("\t=================");
+            Console.WriteLine("\t| Jenis Printer |");
+            Console.WriteLine("\t=================");
+            Console.WriteLine("\t| 1. Canon      |");
+            Console.WriteLine("\t| 2. Epson      |");
+            Console.WriteLine("\t| 3. Laser Jet  |");
+            Console.WriteLine("\t=================\n");
+            Console.Write("\tPilihan Printer (1-3) : ");
+            int pilihPrinter = Convert.ToInt32(Console.ReadLine());
+
+            if (pilihPrinter == 1)
+                Printer = new Canon();
+            else if (pilihPrinter == 2)
+                Printer = new Epson();
+            else
+                Printer = new LaserJet();
+
+            Printer.Show();
+            Printer.Print();
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
